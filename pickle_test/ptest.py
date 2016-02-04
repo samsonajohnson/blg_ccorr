00c66ca8add0bb933d76c1528bb9973ac19750ac
samsonajohnson <samson.a.johnson@gmail.com>
2016-02-04 07:22:07 -0500

    trying to save multi_specs to avoid redoing all the splines

---
+++ pickle_test/ptest.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy
+import scipy.interpolate
+import ipdb
+import pickle
+
+class test_class:
+    def __init__(self,arg1,arg2,arg3,filename = './test.pickle'):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.x = np.linspace(0,2*np.pi,100)
+        self.y = np.sin(self.x)
+        self.testspline = scipy.interpolate.interp1d(self.x,self.y,kind='cubic')
+
+    def foo(self):
+        print self.arg1
+    def bar(self):
+        print self.arg1+self.arg2
+
+    def pickle(self,filename = './test.pickle'):
+        with file(filename,'wb') as f:
+            pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
+
+    def unpickle(self,filename = './test.pkl'):
+       with file('test,pkl', 'rb') as f:
+            return pickle.load(f)
+        
+
+
+                     
